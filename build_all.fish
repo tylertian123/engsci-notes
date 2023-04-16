5969c37770baba7aeb37e21a74c8103002b9551c
Tyler Tian <tylertian123@gmail.com>
2023-04-16 16:53:44 -0400

    Fix build_all.fish script

--- build_all.fish
+++ build_all.fish
@@ -1,6 +1,13 @@
 #!/usr/bin/fish
 
+# Build all Markdown notes files recursively
+
 for md in **/*.md
-	pandoc $md -o (string split -r -m 1 . $md)[1].pdf -V geometry=margin=1in -H /home/tyler/Documents/mateng/ocw/ncommon.tex
+	set md_dir (realpath (dirname $md))
+	set md_name (basename $md)
+	# Temporarily cd to make relative paths work
+	pushd $md_dir
+	pandoc $md_name -o (string split -r -m 1 . $md_name)[1].pdf -V geometry=margin=1in -H /home/tyler/Documents/mateng/ocw/ncommon.tex
+	popd
 end
 
