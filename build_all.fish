b861da5fc970274d69d007b173942f71ad08baa3
Tyler Tian <tylertian123@gmail.com>
2023-09-09 00:33:44 -0400

    Fix build all script to not use absolute header path

--- build_all.fish
+++ build_all.fish
@@ -2,12 +2,14 @@
 
 # Build all Markdown notes files recursively
 
+set header_file (realpath (dirname (status --current-filename)))"/ncommon.tex"
+
 for md in **/*.md
 	set md_dir (realpath (dirname $md))
 	set md_name (basename $md)
 	# Temporarily cd to make relative paths work
 	pushd $md_dir
-	pandoc $md_name -o (string split -r -m 1 . $md_name)[1].pdf -V geometry=margin=1in -H /home/tyler/Documents/mateng/ocw/ncommon.tex
+	pandoc $md_name -o (string split -r -m 1 . $md_name)[1].pdf -V geometry=margin=1in -H $header_file
 	popd
 	echo "Built $md"
 end
