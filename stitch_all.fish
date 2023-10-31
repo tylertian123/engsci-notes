a24968f3b475ed2d2f62f8dffee737241506eeef
Tyler Tian <tylertian123@gmail.com>
2023-10-30 22:38:05 -0400

    Modify script to build one semester

--- stitch_all.fish
+++ stitch_all.fish
@@ -2,7 +2,18 @@
 
 set stitch_dir $PWD
 
-for course in (find . -type d -regextype egrep -regex '.*/[0-9]{4}_(fall|winter)/[a-z]{3}[0-9]{3}_[^/]+')
+set dirs (find . -type d -regextype egrep -regex '.*/[0-9]{4}_(fall|winter)/[a-z]{3}[0-9]{3}_[^/]+')
+
+if not test -n "$dirs"
+	set dirs (find . -type d -regextype egrep -regex './[a-z]{3}[0-9]{3}_[^/]+')
+	set stitch_dir "$PWD/.."
+	if not test -n "$dirs"
+		echo "No directories found."
+		exit 1
+	end
+end
+
+for course in $dirs
     echo "Building $course"
     pushd $course
     $stitch_dir/stitch
